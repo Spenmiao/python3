76d924a6cbfa6d13d2820dc575c6423d81e7a9a8
Spenmiao <mevango@163.com>
2018-04-09 21:15:51 +0800

    递归数组排序

---
+++ misc/递归方法数组排序.py
@@ -0,0 +1,14 @@
+
+nums = [3, 2, 4, 5, 1, 9, 10, 6, 8, 7, 111]
+# # target
+def funsort(nums):
+	l = []
+	for i, j in enumerate(nums[1:]):
+		if nums[0] > j:
+			nums[0] , nums[i+1] = nums[i+1], nums[0]
+	l.append(nums[0])
+	# print(nums)
+	if len(nums) == 2:
+		return nums
+	return l + funsort(nums[1:])
+print(funsort(nums))
